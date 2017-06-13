8aaa1915539919e0b9547d635a7f9f80d1ac95a6
Ransome Mpini <ransome.mpini@bbc.co.uk>
2017-06-13 13:14:01 +0100

    remove pipes

--- R/get_rate.R
+++ R/get_rate.R
@@ -2,17 +2,22 @@ rr <- function(){
     page.url <- 'http://change-link.com/exchange-rates/currency-converter/'
     table.id <- '#ContentPlaceHolderDefault_SidebarZone_Item4_AllRates_8_RatesGridView'
 
-    data.rows <- read_html(page.url) %>% html_node(css = table.id) %>% html_nodes('tr')
-    data.rows <- data.rows[-1]
-    tmpdf <- data.rows %>% lapply(function(curr.row) {
-        td <- curr.row %>% html_nodes('td')
-        data.frame(currency.name = td[2] %>% html_text, buy.val = td[3] %>% html_text, sell.val = td[4] %>% html_text, stringsAsFactors = F)
-    }) %>% bind_rows
-    tmpdf %>% return
-}
-getcurrency <- function(curr.name) rr() %>% filter(grepl(curr.name, currency.name, ignore.case = T))
+    data.table <- xml2::html_node(rvest::read_html(page.url), css = table.id)
+    data.rows <- rvest::html_nodes(data.table, 'tr')
 
-get_rate <- function(curr) c('name:', 'buy:', 'sell:') %>% paste(getcurrency(curr) %>% c, sep='', collapse = ', ')
+    data.rows <- data.rows[-1]
+    tmpdf <-  dplyr::bind_rows(lapply(data.rows, function(curr.row) {
+        td <-  html_nodes(curr.row, 'td')
+        data.frame(currency.name = rvest::html_text(td[2]),
+                   buy.val = rvest::html_text(td[3]),
+                   sell.val = rvest::html_text(td[4]),
+                   stringsAsFactors = F)
+    }))
+    return(tmpdf)
+}
+getcurrency <- function(curr.name)  dplyr::filter(rr(), grepl(curr.name, currency.name, ignore.case = T))
+
+get_rate <- function(curr) c(paste(c('name:', 'buy:', 'sell:'), getcurrency(curr)), sep='', collapse = ', ')
 
 # getrate('united states dollar')
 # getrate('rand')
